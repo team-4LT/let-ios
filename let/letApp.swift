b322a9482a785fd4c2af200f601aba7acfd82518
cher1shRXD <taewoo080401@gmail.com>
2025-04-14 11:44:20 +0900

    Initial Commit

---
+++ let/letApp.swift
@@ -0,0 +1,17 @@
+//
+//  letApp.swift
+//  let
+//
+//  Created by cher1shRXD on 4/14/25.
+//
+
+import SwiftUI
+
+@main
+struct letApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
