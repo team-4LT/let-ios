b7a151275ba02b6073b9d8dc6af8b8369896e5dc
cher1shRXD <taewoo080401@gmail.com>
2025-04-14 15:19:22 +0900

    feat: login bg

--- let/Source/Views/Page/Login.swift
+++ let/Source/Views/Page/Login.swift
@@ -10,23 +10,33 @@ import FlexibleKit
 
 struct Login: View {
     var body: some View {
-        VStack{
-            Spacer()
-            VStack(alignment: .leading){
-                Button {
-                    UserDefaults.standard.set("accessToken", forKey: "accessToken")
-                } label: {
-                    Text("Login")
+        ZStack{
+            VStack {
+                Image("Stars")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: UIScreen.main.bounds.width, height: 220)
+                Spacer()
+            }.ignoresSafeArea(.all)
+            VStack{
+                Spacer()
+                VStack(alignment: .leading){
+                    Button {
+                        UserDefaults.standard.set("accessToken", forKey: "accessToken")
+                    } label: {
+                        Text("Login")
+                    }
                 }
+                .frame(maxWidth: .infinity, maxHeight: UIScreen.main.bounds.height - 200)
+                .background(Color.white)
+                .roundedCorners(20, corners: [.topLeft, .topRight])
+                
             }
-            .frame(maxWidth: .infinity, maxHeight: UIScreen.main.bounds.height - 240)
-            .background(Color.white)
-            .roundedCorners(20, corners: [.topLeft, .topRight])
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .ignoresSafeArea(.all)
             
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .ignoresSafeArea(.all)
-        .background(Color.black)
+        
         
     }
 }
--- let/Source/Views/Page/Profile.swift
+++ let/Source/Views/Page/Profile.swift
@@ -9,7 +9,32 @@ import SwiftUI
 
 struct Profile: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            VStack {
+                Image("Stars")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: UIScreen.main.bounds.width, height: 220)
+                Spacer()
+            }.ignoresSafeArea(.all)
+            VStack{
+                Spacer()
+                VStack(alignment: .leading){
+                    Button {
+                        UserDefaults.standard.removeObject(forKey: "accessToken")
+                    } label: {
+                        Text("Logout")
+                    }
+                }
+                .frame(maxWidth: .infinity, maxHeight: UIScreen.main.bounds.height - 200)
+                .background(Color.white)
+                .roundedCorners(20, corners: [.topLeft, .topRight])
+                
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .ignoresSafeArea(.all)
+            
+        }
     }
 }
 
