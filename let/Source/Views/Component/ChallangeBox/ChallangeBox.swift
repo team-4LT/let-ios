2f22538e7eed29ca013ab8618005e9c11105f525
tjalsejr <minori5529@naver.com>
2025-04-16 19:20:18 +0900

    feat: main view

---
+++ let/Source/Views/Component/ChallangeBox/ChallangeBox.swift
@@ -0,0 +1,18 @@
+//
+//  ChallangeBox.swift
+//  let
+//
+//  Created by dgsw16 on 4/16/25.
+//
+
+import SwiftUI
+
+struct ChallangeBox: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChallangeBox()
+}
---
+++ let/Source/Views/Component/DateCalendar/DateCalendar.swift
@@ -0,0 +1,18 @@
+//
+//  DateCalendar.swift
+//  let
+//
+//  Created by dgsw16 on 4/16/25.
+//
+
+import SwiftUI
+
+struct DateCalendar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DateCalendar()
+}
