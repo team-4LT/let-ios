57ea6416c12822588ca6f6fee24aa8a6d5a5dcaa
imhyeonu826 <166387038+imhyeonu826@users.noreply.github.com>
2025-05-13 09:44:03 +0900

    feat: TimerVIew

--- let/Source/Views/Component/Timer/TimerView.swift
+++ let/Source/Views/Component/Timer/TimerView.swift
@@ -10,31 +10,58 @@ import Combine
 
 struct TimerView: View {
     let date = Date()
-    @State var time: Int = 120
+    @State var time: Int = 180
     @State var timeRemaining : Int = 1
     @State var timer = Timer.publish(every: 1, on: .main, in: .common)
     @State var cancellable: Cancellable?
     var body: some View {
         VStack{
             ZStack {
-                Text(convertSecondsToTime(timeInSeconds:timeRemaining))
-                    .font(.system(size: 50))
-                    .onReceive(timer) { _ in
-                        if timeRemaining > 0 {
-                            timeRemaining -= 1
-                        } else {
-                            cancellable?.cancel()
-                        }
-                    }
-            }
-            Text("가볍게 뛰며")
-                .font(.system(size: 28, weight: .semibold))
-            Text("음식 소화를 시켜보아요!")
-                .font(.system(size: 28, weight: .semibold))
-                .onAppear {
-                    calcRemain()
-                    cancellable = timer.connect()
+                Image("light")
+                    .frame(width: 442, height: 442)
+                VStack {
+                    Text("가볍게 뛰며")
+                        .font(.system(size: 28, weight: .semibold))
+                    Text("음식 소화를 시켜보아요!")
+                        .font(.system(size: 28, weight: .semibold))
+                        .padding(.bottom, 50)
+                    Image("Girlrunning")
+                        .resizable()
+                        .edgesIgnoringSafeArea(.all)
+                        .frame(width: 179, height: 289)
                 }
+
+            }
+            ZStack {
+                HStack {
+                    Image("alarm")
+                        .resizable()
+                        .edgesIgnoringSafeArea(.all)
+                        .frame(width: 30, height: 28)
+                    Text(convertSecondsToTime(timeInSeconds:timeRemaining))
+                        .font(.system(size: 36, weight: .semibold))
+                        .onReceive(timer) { _ in
+                            if timeRemaining > 0 {
+                                timeRemaining -= 1
+                            } else {
+                                cancellable?.cancel()
+                            }
+                        }
+                        .onAppear {
+                            calcRemain()
+                            cancellable = timer.connect()
+                        }
+                }
+            }
+            Button(action: {
+            }) {
+                Text("시작하기")
+                    .font(.system(size: 16, weight: .semibold))
+                    .frame(width: 344, height: 55)
+                    .foregroundStyle(Color(.white))
+                    .background(Color(.red))
+                    .cornerRadius(8)
+            }
         }
     }
         
